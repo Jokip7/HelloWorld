b27a8a070e609eba6ef28ee25167772c2a2a7b45
Stefan van der Ende <scende@avans.nl>
2016-04-20 14:45:06 +0200

    a

--- src/huiswerk/Main.java
+++ src/huiswerk/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("‚r‚l‚`‚k‚k@‚k‚n‚`‚m@‚n‚e@‚`@‚l‚h‚k‚k‚h‚n‚m@‚c‚n‚k‚k‚`‚q‚r");
+		
 		MyGui gui = new MyGui();
 		gui.create();
 
