6fd84de72354c851655fe4f28349587bac524433
Stefan van der Ende <scende@avans.nl>
2016-04-20 15:07:32 +0200

    got fired

--- src/text/Main.java
+++ src/text/Main.java
@@ -2,6 +2,9 @@ package text;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		System.out.println("‚r‚l‚`‚k‚k@‚k‚n‚`‚m@‚n‚e@‚`@‚l‚h‚k‚k‚h‚n‚m@‚c‚n‚k‚k‚`‚q‚r");
+		
 		@SuppressWarnings("unused")
 		MyGui myGui = new MyGui();
 	}
